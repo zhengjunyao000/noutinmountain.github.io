2e35391946fd9b99aed98779d1a4d3ac09a789e8
noutinmountain <1060477485@qq.com>
2022-08-14 17:03:08 +0800

    Site updated: 2022-08-14 17:03:07

--- js/diytitle.js
+++ js/diytitle.js
@@ -4,7 +4,7 @@
  * @Autor: zheng
  * @Date: 2021-12-31 15:37:34
  * @LastEditors: zheng
- * @LastEditTime: 2021-12-31 16:25:16
+ * @LastEditTime: 2022-08-14 17:00:32
  */
 //动态标题
 var OriginTitile = document.title;
@@ -12,12 +12,12 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         //离开当前页面时标签显示内容
-        document.title = '已离开页面';
+        document.title = '离开页面';
         clearTimeout(titleTime);
     }
     else {
         //返回当前页面时标签显示内容
-        document.title = '欢迎回来 ' + OriginTitile;
+        document.title = '欢迎回来 ';
         //两秒后变回正常标题
         titleTime = setTimeout(function () {
             document.title = OriginTitile;
